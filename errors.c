e4bfec9b7ab3cf4f441e424525589616b67647af
Sixtus <sixtus4real2k2@ymail.com>
2023-03-08 13:28:37 +0100

    Update errors.c

--- errors.c
+++ errors.c
@@ -3,6 +3,7 @@
 /**
  *_eputs - prints an input string
  * @str: the string to be printed
+ *
  * Return: Nothing
  */
 void _eputs(char *str)
@@ -21,6 +22,7 @@ void _eputs(char *str)
 /**
  * _eputchar - writes the character c to stderr
  * @c: The character to print
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -41,8 +43,9 @@ int _eputchar(char c)
 
 /**
  * _putfd - writes the character c to given fd
- * @c: character
- * @fd: integer
+ * @c: The character to print
+ * @fd: The filedescriptor to write to
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -64,7 +67,8 @@ int _putfd(char c, int fd)
 /**
  *_putsfd - prints an input string
  * @str: the string to be printed
- * @fd: the filedescriptor to write
+ * @fd: the filedescriptor to write to
+ *
  * Return: the number of chars put
  */
 int _putsfd(char *str, int fd)

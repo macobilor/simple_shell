0d73128ace70a743330c7942653caf711b6414d8
Sixtus <sixtus4real2k2@ymail.com>
2023-03-08 13:24:18 +0100

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -1,8 +1,10 @@
 #include "shell.h"
 
 /**
- * _myhistory - displays the history list, one command by line.
- * @info: Structure containing potential arguments.
+ * _myhistory - displays the history list, one command by line, preceded
+ *              with line numbers, starting at 0.
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
  *  Return: Always 0
  */
 int _myhistory(info_t *info)
@@ -15,6 +17,7 @@ int _myhistory(info_t *info)
  * unset_alias - sets alias to string
  * @info: parameter struct
  * @str: the string alias
+ *
  * Return: Always 0 on success, 1 on error
  */
 int unset_alias(info_t *info, char *str)
@@ -37,6 +40,7 @@ int unset_alias(info_t *info, char *str)
  * set_alias - sets alias to string
  * @info: parameter struct
  * @str: the string alias
+ *
  * Return: Always 0 on success, 1 on error
  */
 int set_alias(info_t *info, char *str)
@@ -56,6 +60,7 @@ int set_alias(info_t *info, char *str)
 /**
  * print_alias - prints an alias string
  * @node: the alias node
+ *
  * Return: Always 0 on success, 1 on error
  */
 int print_alias(list_t *node)
@@ -77,7 +82,8 @@ int print_alias(list_t *node)
 
 /**
  * _myalias - mimics the alias builtin (man alias)
- * @info: Structure containing potential arguments.
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
  *  Return: Always 0
  */
 int _myalias(info_t *info)

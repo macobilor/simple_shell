da08a3c0775f5920c4d7b1e56c6e1b62ab25d4d2
Sixtus <sixtus4real2k2@ymail.com>
2023-03-08 13:16:42 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,17 +1,17 @@
 #include "shell.h"
 
 /**
- * _myexit - exit the shell
+ * _myexit - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- * Return: exits with a given exit status
- * (0) if info.argv[0] != "exit"
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
 	int exitcheck;
 
-	if (info->argv[1]) /* if there is an exit argument */
+	if (info->argv[1])  /* If there is an exit arguement */
 	{
 		exitcheck = _erratoi(info->argv[1]);
 		if (exitcheck == -1)
@@ -32,8 +32,8 @@ int _myexit(info_t *info)
 /**
  * _mycd - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- * Return: Always 0
+ *          constant function prototype.
+ *  Return: Always 0
  */
 int _mycd(info_t *info)
 {
@@ -66,24 +66,24 @@ int _mycd(info_t *info)
 	}
 	else
 		chdir_ret = chdir(info->argv[1]);
-				if (chdir_ret == -1)
-				{
-					print_error(info, "can't cd to ");
-					_eputs(info->argv[1]), _eputchar('\n');
-				}
-				else
-				{
-					_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-					_setenv(info, "PWD", getcwd(buffer, 1024));
-				}
-				return (0);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
 }
 
 /**
  * _myhelp - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- * Return: Always 0
+ *          constant function prototype.
+ *  Return: Always 0
  */
 int _myhelp(info_t *info)
 {
